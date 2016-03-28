29d6a4165aeb611ec048f93c1e389561b80324e9
nanobeauty <nanobeauty@naver.com>
2016-03-28 21:40:14 +0700

    1. 라우터 설정하기. 로그인 설정하기 p187(모던 웹을 위한 Node.js 프로그래밍). 의 변형

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (request, response) {
+    if (request.cookies.auth) {
+        response.send('<h1>Login Success</h1>');
+    } else {
+        response.redirect('/login');
+    }
+});
+router.get('/login', function (request, response) {
+    fs.readFile('../public/login/index.html', function (error, data) {
+        response.send(data.toString());
+    });
+});
+router.post('/login', function (request, response) {
+    // 쿠키를 생성합니다.
+    var login = request.param('login');
+    var password = request.param('password');
+
+    // 출력합니다.
+    console.log(login, password);
+    console.log(request.body);
+
+    // 로그인을 확인합니다.
+    if (login == 'rint' && password == '1234') {
+        // 로그인 성공
+        response.cookie('auth', true);
+        response.redirect('/');
+    } else {
+        // 로그인 실패
+        response.redirect('/login');
+    }
 });
 
 module.exports = router;
